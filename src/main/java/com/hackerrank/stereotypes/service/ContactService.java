a5fa3dcd8d946f739f5c06970bc8a8daf5fd7cdb
Anusha-nidamanuru <90374133+Anusha-nidamanuru@users.noreply.github.com>
2021-09-09 12:01:45 +0530

    Update ContactService.java

--- src/main/java/com/hackerrank/stereotypes/service/ContactService.java
+++ src/main/java/com/hackerrank/stereotypes/service/ContactService.java
@@ -2,8 +2,13 @@ package com.hackerrank.stereotypes.service;
 
 import com.hackerrank.stereotypes.model.Person;
 import com.hackerrank.stereotypes.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ContactService {
+    
+    @Autowired
     ContactRepository contactRepository;
 
     public Person retrieve(Integer id){
